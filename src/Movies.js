1eceb4f5f63e8602e35bd07cbfeb6171b0eabee3
oliverlai55 <oliverlai55@gmail.com>
2017-04-23 22:51:02 -0400

    Add styling

--- src/Movies.js
+++ src/Movies.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import {
   ScrollView,
-  Text,
+  StyleSheet,
   View
 } from 'react-native';
 import { movies } from './data';
+import MoviePoster from './MoviePoster';
 
 export default class Movies extends Component {
   render() {
     return (
-      <View>
-        <ScrollView>
-          {movies.map((movie, index) => <Text>{movie.title}</Text>)}
+      <View style={styles.container}>
+        <ScrollView
+          contentContainerStyle={styles.scrollContent}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+        >
+          {movies.map((movie, index) => <MoviePoster
+            movie={movie}
+            onOpen={this.openMovie}
+            key={index}
+          />)}
         </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20        // start below status bar
+  },
+  scrollContent: {
+    flexDirection: 'row', // arrange posters in rows
+    flexWrap: 'wrap'      // allow multiple rows
+  }
+});
