496bfe40bc2cf62a151f1610647a6122a50b74b8
oliverlai55 <oliverlai55@gmail.com>
2017-04-28 00:05:50 -0400

    Fix routing

--- src/app.js
+++ src/app.js
@@ -3,10 +3,17 @@ import {
   Navigator
 } from 'react-native';
 import Movies from './Movies';
+import Confirmation from './Confirmation';
 
 const RouteMapper = (route, navigator) => {
   if (route.name === 'movies') {
-    return <Movies navigator={navigator} />;
+    return (
+      <Movies navigator={navigator} />
+    );
+  } else if (route.name === 'confirmation') {
+    return (
+      <Confirmation code={route.code} navigator={navigator} />
+    );
   }
 };
 
